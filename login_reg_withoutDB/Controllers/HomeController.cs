41ac0263ec23e05bb1c3d255852667fd4a65c85a
Alishamhaske <mmhaske2001@gmail.com>
2024-03-09 18:24:50 +0530

    Add project files.

---
+++ login_reg_withoutDB/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using login_reg_withoutDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace login_reg_withoutDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            HttpContext.Session.SetString("email", user.Email);
+            HttpContext.Session.SetString("password", user.Password);
+            return RedirectToAction("Login");
+
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            string email=HttpContext.Session.GetString("email");
+            string password=HttpContext.Session.GetString("password");
+            if(email==user.Email && password ==user.Password)
+            {
+                ViewBag.Msg = "Login sucessfully";
+            }
+            else
+            {
+                ViewBag.Msg = "Login Failed";
+            }
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ login_reg_withoutDB/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace login_reg_withoutDB.Models
+{
+    public class User
+    {
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(30)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+
+    }
+}
